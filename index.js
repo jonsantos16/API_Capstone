77d1cb7d6b1f34c2b93f632581406073de659fcf
Jonathan Santos <santosjn94@gmail.com>
2018-05-29 16:55:31 -0700

    finished js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Map_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 const Hike_URL = 'https://www.hikingproject.com/data/get-trails';
-const Weather_URL = 'https://api.weatherbit.io/v2.0/forecast/daily'
+const Weather_URL = 'https://api.weatherbit.io/v2.0/forecast/daily';
 
 let geoCode = {
     address: '',
@@ -11,7 +11,7 @@ let hikeQuery = {
     key: '200265082-fbc1bc2d3aae4542c7fdc5335e6d16b9',
     lat: '',
     lon: '',
-    maxResults: 2,
+    maxResults: 3,
 };
 
 let weatherQuery = {
@@ -21,6 +21,8 @@ let weatherQuery = {
 };
 
 let info = [];
+let today = [];
+let tmrw = [];
 
 
 const search = () => {
@@ -48,7 +50,6 @@ const getHikeData = (data) => {
         data.trails.forEach(trail => {
             weatherQuery.lat = trail.latitude;
             weatherQuery.lon = trail.longitude;
-            
             getWeatherData();
         })
     });
@@ -58,8 +59,13 @@ const getWeatherData = () => {
     $.getJSON(Weather_URL, weatherQuery, (data) => {
         // console.log(data.data);
         pushWeatherData(data.data)
-        // displayWeatherData(data);
     });
+}
+
+const showQueries = () => {
+    // console.log(info);
+    // console.log(weather_now);
+    // console.log(weather_tmrw);
 }
 
 const pushHikeData = trail => {
@@ -71,60 +77,77 @@ const pushHikeData = trail => {
             length: `${trail[i].length} mi`,
             img: `${trail[i].imgSmallMed}`,
             difficulty: `${trail[i].difficulty}`,
-            weather_today: {},
-            weather_tmrw: {},
         });
     }
-    console.log(info);
+    // console.log(info);
 }
 
 const pushWeatherData = item => {
-    console.log(item);
-    info[weather_today].push('hello');
+    // console.log(item);
+    today.push({
+        high: `${item[0].max_temp}`,
+        low: `${item[0].min_temp}`,
+        wind_speed: `${item[0].wind_spd} m/s`,
+        prob_of_precip: `${item[0].pop}`,
+        weather_icon: `${item[0].weather.icon}`,
+        weather_code: `${item[0].weather.code}`,
+        weather_description: `${item[0].weather.description}`
+    });
+    
+    tmrw.push({
+        high: `${item[1].max_temp}`,
+        low: `${item[1].min_temp}`,
+        wind_speed: `${item[1].wind_spd} m/s`,
+        prob_of_precip: `${item[1].pop}`,
+        weather_icon: `${item[1].weather.icon}`,
+        weather_code: `${item[1].weather.code}`,
+        weather_description: `${item[1].weather.description}`
+    })
+    
+    getItemsHtml();
+    // showQueries();
 }
 
-const displayHikeData = data => {
+const getItemsHtml = () => {
+    let trailDiv = '';
+    for (i = 0; i < hikeQuery.maxResults; i++) {
+        trailDiv += `
+            <div class="row">
+                <div class="hike-result">
+                    <img src="${info[i].img}" alt="picture of ${info[i].name}">
+                    <h2>${info[i].name}</h2>
+                    <h3>${info[i].length}</h3>
+                    <h3>${info[i].difficulty}</h3>
+                    <h3>${info[i].location}</h3>
+                </div>
+                <div class="weather-result">
+                    <div class="forecast today">
+                        <h3>Today's weather at ${info[i].location}:</h3>
+                        <h4>High: ${today[i].high}</h4>
+                        <h4>Low: ${today[i].low}</h4>
+                        <h4>Probability of Precipitation: ${today[i].prob_of_precip}</h4>
+                        <h4>Wind Speed: ${today[i].wind_speed}</h4>
+                        <h4>Description: ${today[i].weather_description}</h4>
+                    </div>
+                    <div class="forecast tmrw">
+                        <h3>Tomorrow's weather at ${info[i].location}:</h3>
+                        <h4>High: ${tmrw[i].high}</h4>
+                        <h4>Low: ${tmrw[i].low}</h4>
+                        <h4>Probability of Precipitation: ${tmrw[i].prob_of_precip}</h4>
+                        <h4>Wind Speed: ${tmrw[i].wind_speed}</h4>
+                        <h4>Description: ${tmrw[i].weather_description}</h4>
+                    </div>
+                </div>
+            </div>`;
+
+    }
+    console.log(trailDiv);
+    displayData(trailDiv)
+}
+
+const displayData = (trailDiv) => {
     // console.log('data dot trails is ', data.trails);
-    const results = getItemsHtml(data.trails);
-    $('.results').html(results);
-}
-
-const getItemsHtml = trails => (
-    trails.map((trail) => (
-        `<div>
-            <a href="${trail.url} target="blank">
-                <img src="${trail.imgSmallMed}" alt="picture of ${trail.name}">
-            </a>
-            <a href="${trail.url} target="blank">
-                <h2>${trail.name}</h2>
-            </a>
-            <h3>${trail.location}</h3>
-            <h4>Length: ${trail.length} miles</h4>
-        </div>`
-    ))
-)
-
-
-
-const displayWeatherData = data => {
-    console.log ('weather data is ', data);
-    // console.log ('weather data.data is ', data.data);
-    let sixteenDay = getWeatherHtml(data);
-    console.log(sixteenDay);
-    // getWeatherHtml(data);
-    $('.results').append(sixteenDay);
-}
-
-const getWeatherHtml = (data) => {
-    let array = [];
-    data.data.map(day => {
-        array.push(    
-            `<div>
-            <h3>${day.max_temp}</h3>
-            </div>`
-        );    
-    }) 
-    return array;     
+    $('.results').html(trailDiv);
 }
 
 const initApp = () => {
